e7a6a462b3e02c88d55e100da3df384ab2c2639f
rockeychauhan1 <84220993+rockeychauhan1@users.noreply.github.com>
2022-05-30 17:29:02 +0530

    commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = "ME";
+const botName = "";
 
 // Run when client connects
 io.on('connection', socket => {
