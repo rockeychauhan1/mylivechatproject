442e29d4554e95987b457fd2bd1ee9dd56092760
rockeychauhan1 <84220993+rockeychauhan1@users.noreply.github.com>
2022-05-31 11:41:40 +0530

    hii01

--- main.js
+++ main.js
@@ -2,15 +2,12 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
-const socketio = require('socket.io');
+
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
 const socket = io();
 
 // Join chatroom
