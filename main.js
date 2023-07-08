409084d5e5caffc7a44dabeda718523a22d0aa60
unknown <chauhan786rockey@gmail.com>
2023-07-08 11:37:55 +0530

    updation

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
-import * as io from 'socket.io-client';
+
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
